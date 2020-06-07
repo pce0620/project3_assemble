1d62511c04b525a00f7242175b7f74345efadf1b
goott7-1 <goott7-1@DESKTOP-DV1FNDT>
2020-06-07 19:53:34 +0900

    n

---
+++ assemble/src/main/java/kr/co/assemble/dto/RecommentDTO.java
@@ -0,0 +1,19 @@
+package kr.co.assemble.dto;
+
+import java.util.Date;
+
+public class RecommentDTO {
+	private int reno;
+	private int bno;
+	private int groupno;
+	private int categoryno;
+	private int memberno;
+	private Date redate;
+	private String reid;
+	
+	public RecommentDTO() {}
+	
+	
+	
+	
+}
